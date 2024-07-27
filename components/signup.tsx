07516cfaf8f69f8e605772e03a5d7615a9544e03
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-07-27 13:23:19 +0530

    feat: signup card

--- components/signup.tsx
+++ components/signup.tsx
@@ -7,6 +7,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
 
 const Signup = () => {
   return (
@@ -15,11 +16,17 @@ const Signup = () => {
         <div className="width-[528px] height-[58px] font-semibold font-Barlow text-[48px] text-center">
           Welcome to <span className="text-[#5539f0]">Workflo</span>!
         </div>
-              <div className="w-[528px] h-[290px] flex flex-col gap-[42px] mt-4">
-                  <Textarea className=""/>
-                  <div>2</div>
-                  <div>3</div>
-                  <div></div>
+        <div className="w-[528px] h-[290px] flex flex-col gap-[30px] mt-4">
+          <Textarea placeholder="Full name" />
+          <Textarea placeholder="Your email" />
+          <Textarea placeholder="Password" />
+          <Button className="text-[20px] font-normal text-[#FFFFFF]">
+            Sign up
+          </Button>
+        </div>
+        <div className="h-[24px] font-normal text-[20px] text-center text-[#606060] mt-4 ">
+          Already have an account?<span className="text-[#0054A1]">Log in</span>
+          .
         </div>
       </Card>
     </div>
