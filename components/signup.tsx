2afc21a45cdac4b7975c3bbde47d74406c2526fc
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-07-27 11:18:13 +0530

    refact: card

--- components/signup.tsx
+++ components/signup.tsx
@@ -1,9 +1,20 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const Signup = () => {
-    return ( 
-        <div>
-            signup
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <Card className="w-[648px] h-[556px] mt-[90px] ml-[396px] rounded-[16px] p-[60px]">
+        <div className="">Welcome to Workflo!</div>
+      </Card>
+    </div>
+  );
+};
+
 export default Signup;
