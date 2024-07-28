5d1b6114b1a9b201c5faf3ac7c45caa0d1740a15
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-07-28 23:56:12 +0530

    lint

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 
 const Signup = () => {
   return (
-    <div className="w-screen h-screen bg-gradient-to-b from-[#FFFFFF] to-[#AFA3FF]">
-      <Card className="w-[648px] h-[556px] mt-20 ml-[396px] rounded-[16px] p-[60px] bg-gradient-to-b from-[#F7F7F7] to-[#F0F0F0]">
+    <div className="w-screen h-screen bg-gradient-to-b from-[#FFFFFF] to-[#AFA3FF] flex items-center justify-center fixed">
+      <Card className="w-[648px] h-[556px] rounded-[16px] p-[60px] bg-gradient-to-b from-[#F7F7F7] to-[#F0F0F0]">
         <div className="width-[528px] height-[58px] font-semibold font-Barlow text-[48px] text-center">
           Welcome to <span className="text-[#5539f0]">Workflo</span>!
         </div>
