35ef2372711fe0860317761a328a1573ef13c2bd
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-07-29 11:25:09 +0530

    refact: userinfo

--- components/head.tsx
+++ components/head.tsx
@@ -9,10 +9,12 @@ const Userinfo = () => {
                 <div className="font-normal text-[20px] ">Joe Gardener</div>
             </div>
             <div className="flex justify-between">
-                <div>
+                <div className="flex gap-[20px]">
                     <Image src={"/icon1.svg"} alt="" width={24} height={24} />
+                    <Image src={"/icon2.svg"} alt="" width={24} height={24} />
+                    <Image src={"/icon3.svg"} alt="" width={24} height={24} />
                 </div>
-                <div></div>
+                <div className="bg-[#F4F4F4] w-[69px] h-[40px] rounded-[4px] p-[8px] font-normal text-[16px] text-[#797979]">Logout</div>
             </div>
             
           
