ba36006808d8fb574a1d728a3508a6212cc5e009
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-07-29 14:58:26 +0530

    feat: sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen border-r bg-background flex flex-col justify-between shadow-md pt-[24px] pr-[16px] pb-[32px] pl-[16px] gap-[16px]">
+    <div className="h-full fixed border-r bg-background flex flex-col justify-between shadow-md pt-[24px] pr-[16px] pb-[32px] pl-[16px] gap-[16px]">
       <div>
         <div className="w-[253px] h-[79px] gap-[8px]">
           <Userinfo />
@@ -16,10 +16,32 @@ const Sidebar = () => {
             <Sidebaritems key={idx} image={item.image} content={item.content} />
           ))}
         </div>
-              <Button className="text-[20px] text-[#FFFFFF] font-normal mt-2" variant={"destructive"} size={"sm"}>Create new task <span className="ml-1"><Image src={"/plus.svg"} alt="" width={24} height={24} /></span> </Button>
+        <Button
+          className="text-[20px] text-[#FFFFFF] font-normal mt-2 hover:text-neutral-300"
+          variant={"destructive"}
+          size={"sm"}
+        >
+          Create new task{" "}
+          <span className="ml-1">
+            <Image src={"/plus.svg"} alt="" width={24} height={24} />
+          </span>{" "}
+        </Button>
       </div>
 
-      <div>hello </div>
+      <div className="w-[253px] h-[61px] flex gap-2 bg-[#F3F3F3]">
+        <div className="h-[40px] w-[40] flex flex-col gap-1 mt-1 ml-1 items-center justify-center">
+          <Image src={"/vector.svg"} alt="" height={20} width={11.67} />
+          <Image src={"/line.svg"} alt="" height={1} width={20} />
+        </div>
+        <div className="h-[45] w-[189] gap-[4px] ml-2">
+          <div className="font-medium text-[20px] text-[#666666]">
+            Download the app
+          </div>
+          <div className="font-normal text-[14px] text-[#666666]">
+            Get the full experience{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
